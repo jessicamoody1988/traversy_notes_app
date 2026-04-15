0f3a1f463f870f5024ca554397aea55cda8508d8
Jessica Moody <jessicamoody1988@gmail.com>
2026-04-15 15:41:49 -0500

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { NotepadText } from "lucide-react";
+
+export default function App() {
+  return (
+    <div className="max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg">
+      <div className="flex justify-center items-center gap-2 text-2xl font-bold mb-4 text-center">
+        <NotepadText />
+        <h2>Notes App</h2>
+      </div>
+    </div>
+  );
+}
